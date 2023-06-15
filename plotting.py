b1ca8353129cf37bb2a94fc0f689ffa5c885958a
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-06-15 12:07:05 +0200

    plot update

--- plotting.py
+++ plotting.py
@@ -1,6 +1,7 @@
 from os.path import exists
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from vitaldb import VitalFile
 
 import json
@@ -37,47 +38,71 @@ while plotting:
     vtf = VitalFile(f"{DATA_FOLDER}vital/{caseid}.vital")
     original = vtf.to_pandas(["SNUADC/ART", "Solar8000/ART_MBP"], 1 / 100)
 
-    processesed = pd.read_pickle(
-        f"{DATA_FOLDER}preprocessed/{event}/{caseid}.pkl"
-    )
+    processesed = pd.read_pickle(f"{DATA_FOLDER}preprocessed/{event}/{caseid}.pkl")
 
     if input("Stacked? (y/n)") == "y":
+        original_pre_ag = original[original.index < processesed.index.min()]
+        original_post_ag = original[original.index > processesed.index.max()]
 
-        processesed.reset_index(inplace=True, drop=True)
-
-        mask = original["SNUADC/ART"] > 40
-        rolling_sum = mask.rolling(window=timeframe).sum()
-        ag_start = rolling_sum[rolling_sum >= 0.9 * timeframe].idxmin()
-
-        ag_end = ag_start + len(processesed)
-
-        processesed.index = processesed.index + ag_start
-
+        # Find IOH
         mask = processesed["Solar8000/ART_MBP"].lt(65)
         idxs = processesed.index[
-            mask.rolling(window=60 * SAMPLING_RATE, axis=0).apply(lambda x: x.all(), raw=True)
+            mask.rolling(window=60 * SAMPLING_RATE, axis=0).apply(
+                lambda x: x.all(), raw=True
+            )
             == True
         ]
 
-        original_pre_ag = original.iloc[0:ag_start]
-        original_post_ag = original.iloc[ag_end:]
-        plot = original_pre_ag.plot(y="SNUADC/ART", color="gray", legend=False)
-        original_post_ag.plot(ax=plot, y="SNUADC/ART", color="gray", legend=False)
+        fig, ax = plt.subplots(figsize=(15, 6))
 
-        processesed.plot(ax=plot, y=["SNUADC/ART", "Solar8000/ART_MBP"])
-        # Highlight all segments with < 4
-        if idxs.empty:
-            print("No hypo")
-        else:
+        original_pre_ag.plot(ax=ax, y="SNUADC/ART", color="gray", label=None)
+        original_post_ag.plot(ax=ax, y="SNUADC/ART", color="gray", label=None)
+
+        processesed.plot(
+            ax=ax,
+            y=["SNUADC/ART", "Solar8000/ART_MBP"],
+            label=["Arterial pressure waveform", "Mean arterial pressure"],
+        )
+
+        # Highlight all segments with hypotensive (IOH) events
+        hypotensive_event_patch = mpatches.Patch(
+            color="red", alpha=0.3, label="Hypotensive event (1 minute wide)"
+        )
+
+        if not idxs.empty:
             idx_prev = 0
-            for idx in idxs:
+            for i, idx in enumerate(idxs):
                 if idx - idx_prev >= 60 * SAMPLING_RATE:
-                    plot.axvspan(idx - 60 * SAMPLING_RATE, idx, color="red", alpha=0.3)
-                idx_prev = idx
+                    label = "Hypotensive event (1 minute wide)" if i == 0 else None
+                    ax.axvspan(
+                        idx - 60 * SAMPLING_RATE,
+                        idx,
+                        color="red",
+                        alpha=0.3,
+                        label=label,
+                    )
+                    idx_prev = idx
+        # Add the custom legend
+        current_handles, current_labels = ax.get_legend_handles_labels()
+        current_handles.append(hypotensive_event_patch)
+        ax.legend(handles=current_handles, labels=current_labels)
+        plt.ylim((-50, 200))
 
     else:
-        fig, axs = plt.subplots(2, 1, sharex=True)
-        original.plot(ax=axs[0], y=["SNUADC/ART", "Solar8000/ART_MBP"], title="Original")
-        processesed.plot(ax=axs[1], y=["SNUADC/ART", "Solar8000/ART_MBP"], title="Processed")
+        fig, axs = plt.subplots(2, 1, sharex=True, figsize=(15, 6))
+        original.plot(
+            ax=axs[0],
+            y=["SNUADC/ART", "Solar8000/ART_MBP"],
+            title="Original",
+            label=["Arterial pressure waveform", "Mean arterial pressure"],
+            ylim=(-50, 200),
+        )
+        processesed.plot(
+            ax=axs[1],
+            y=["SNUADC/ART", "Solar8000/ART_MBP"],
+            title="Processed",
+            label=["Arterial pressure waveform", "Mean arterial pressure"],
+            ylim=(-50, 200),
+        )
 
     plt.show()
