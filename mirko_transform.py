712415f1565a9249b69bb6bada771e315513b8aa
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-07-04 11:14:07 +0200

    mirko transform done

--- mirko_transform.py
+++ mirko_transform.py
@@ -22,35 +22,73 @@ def group_beat_unstack(filename, output):
         start_seg = list(changes.index[0::2])
         end_seg = list(changes.index[1::2])
         if len(start_seg) > len(end_seg):
-            end_seg += [-1]
+            end_seg.append(df.index[-1])
 
     event_segments = [
-        list(range(a, b))
-        for a, b in zip(start_seg, end_seg)
-        if b - a > 60 * env.SAMPLING_RATE
+        (a, b) for a, b in zip(start_seg, end_seg) if b - a > 60 * env.SAMPLING_RATE
     ]
-    flat_segments = [item for sublist in event_segments for item in sublist]
+
+    event_segments_shift = [
+        (
+            a - env.PRED_WINDOW * 60 * env.SAMPLING_RATE,
+            b - env.PRED_WINDOW * 60 * env.SAMPLING_RATE,
+        )
+        for a, b in event_segments
+    ]
+
+    # flat_segments = [item for sublist in event_segments for item in sublist]
     # fig, ax = plt.subplots(figsize=(15, 6))
     # ax.plot(df["Solar8000/ART_MBP"])
     # for start, end in event_segments:
     #     ax.axvspan(start, end, alpha=0.1, color="red")
+    # for start, end in event_segments_shift:
+    #     ax.axvline(start, color="purple", alpha=0.5)
     # plt.show()
 
     art_wf = df["SNUADC/ART"]
-    dia_valley = signal.find_peaks(-art_wf.values, distance=40, prominence=10)[0]
+    dia_valley = signal.find_peaks(-art_wf, distance=40, prominence=10)[0]
+
+    dia_valley = [art_wf.index[i] for i in dia_valley]
+
+    beat_event = []
+    i, j = 0, 0
+    while j < len(dia_valley) - 1:
+        while j < len(dia_valley) - 1 and dia_valley[j] < event_segments_shift[i][0]:
+            beat_event.append(False)
+            j += 1
+        while j < len(dia_valley) - 1 and dia_valley[j] < event_segments[i][1]:
+            beat_event.append(True)
+            j += 1
+        i += 1
+        if i == len(event_segments):
+            break
+    beat_event += [False] + [False] * (len(dia_valley) - len(beat_event))
+
+    # fig, ax = plt.subplots(figsize=(15, 6))
+    # ax.plot(df["Solar8000/ART_MBP"])
+    # i = 0
+    # for start, end in event_segments:
+    #     if not i:
+    #         ax.axvspan(start, end, alpha=0.1, color="red", label="IOH")
+    #         i += 1
+    #     ax.axvspan(start, end, alpha=0.1, color="red")
+    # ax.plot(dia_valley, [50 if x else 40 for x in beat_event], "-", label=f"{env.PRED_WINDOW} min before IOH")
+    # ax.axhline(65, color="green", alpha=0.5, label="IOH Threshold")
+    # ax.legend()
+    # plt.show()
 
     df = df[["SNUADC/ART"]]
 
     df["beat"] = df.index.isin(dia_valley)
-    df["beat"] = df["beat"].ne(df["beat"].shift()).cumsum().add(-1)
-
-    df["event"] = df.index.isin(flat_segments)
+    df["beat"] = df["beat"].cumsum()
 
     out = (
         df.assign(value=df.groupby("beat").cumcount())
         .set_index(["beat", "value"])
         .unstack()
     )
+
+    out.insert(loc=0, column="label", value=beat_event)
 
     compression_set = {"method": "gzip", "compresslevel": 1, "mtime": 1}
 
@@ -71,10 +109,6 @@ def group_beat_unstack_multithreaded(ifolder, ofolder):
         executor.map(group_beat_unstack, ifiles, ofiles)
 
 
-group_beat_unstack(
-    join(env.DATA_FOLDER, "preprocessed", "event", "111.gz"),
-    join(env.DATA_FOLDER, "mirko", "111.gz"),
-)
 group_beat_unstack_multithreaded(
-    join(env.DATA_FOLDER, "preprocessed", "event"), join(env.DATA_FOLDER, "mirko")
+    join(env.DATA_FOLDER, "preprocessed", "event"), join(env.DATA_FOLDER, "mirko", "event")
 )
