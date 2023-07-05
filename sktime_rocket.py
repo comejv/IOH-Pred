8d21d8cde47551f7fdfe94feda89e2e8d8c32e40
Come VINCENT <44554692+comejv@users.noreply.github.com>
2023-07-05 14:59:18 +0200

    save rocket fit

--- sktime_rocket.py
+++ sktime_rocket.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sktime.transformations.panel.rocket import (
     MiniRocketMultivariate,
 )
-from sktime.datatypes import check_raise, convert
+from sktime.datatypes import check_raise
 from sklearn.metrics import confusion_matrix
 from sktime.utils import mlflow_sktime
 import seaborn as sns
@@ -28,8 +28,9 @@ if not exists("models/model1/"):
     Y_train = Y_train[Y_train.index.isin(remaining_windows)]
     check_raise(X_train, mtype="pd-multiindex")
 
-    minirocket_multi = MiniRocketMultivariate()
+    minirocket_multi = MiniRocketMultivariate(n_jobs=4)
     minirocket_multi.fit(X_train)
+    minirocket_multi.save("models/model1/estimator")
     X_train_transform = minirocket_multi.transform(X_train)
 
     scaler = StandardScaler(with_mean=False)
@@ -45,8 +46,9 @@ if not exists("models/model1/"):
 else:
     classifier = mlflow_sktime.load_model("models/model1/")
     minirocket_multi = MiniRocketMultivariate()
+    minirocket_multi.load_from_path("models/model1.zip")
     scaler = StandardScaler(with_mean=False)
-    
+
 
 # LOAD TEST DATA
 X_test = (
@@ -57,12 +59,12 @@ X_test = (
 Y_test = pd.read_pickle(join(env.DATA_FOLDER, "ready", "labels", "118_labels.gz"))
 remaining_windows = X_test.index.get_level_values("window").unique()
 Y_test = Y_test[Y_test.index.isin(remaining_windows)]
-minirocket_multi.fit(X_test)
 X_test_transform = minirocket_multi.transform(X_test)
 X_test_scaled_transform = scaler.fit_transform(X_test_transform)
 
 # PREDICTION
 Y_pred = classifier.predict(X_test_scaled_transform)
+Y_scores = classifier.decision_function(X_test_scaled_transform)
 cm = confusion_matrix(Y_test, Y_pred)
 cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
 
@@ -91,10 +93,10 @@ axtr.set_title("Confusion Matrix - Percentages")
 axtr.set_xlabel("Predicted label")
 axtr.set_ylabel("True label")
 
-axb.plot(Y_test, color='blue')
-axb.fill_between(Y_test.indices(), Y_test.values, color='blue', alpha=0.2)
-axb.plot(Y_pred, color='red')
-axb.fill_between(Y_pred.indices(), Y_pred.values, color='red', alpha=0.2)
+axb.plot(Y_test, color="blue")
+# axb.fill_between(Y_test.indices(), Y_test.values, color='blue', alpha=0.2)
+axb.plot(Y_pred, color="red")
+# axb.fill_between(Y_pred.indices(), Y_pred.values, color='red', alpha=0.2)
 axb.set_title("Predictions")
 
 plt.show()
