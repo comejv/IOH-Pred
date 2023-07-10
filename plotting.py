50d3296b66c3b5ff2c6665f8ecbf33a5851c9e4a
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-07-10 17:02:29 +0200

    simplify plotting

--- plotting.py
+++ plotting.py
@@ -31,92 +31,66 @@ while plotting:
         continue
 
     # read pickled dataframes
-    vtf = VitalFile(f"{env.DATA_FOLDER}vital/{caseid}.vital")
-    original = vtf.to_pandas(["SNUADC/ART", "Solar8000/ART_MBP", "Solar8000/ART_DBP", "Solar8000/ART_SBP"], 1 / env.SAMPLING_RATE)
-    original.ffill(inplace=True)
+    preprocessed = pd.read_pickle(
+        f"{env.DATA_FOLDER}preprocessed/{event}/{caseid}.gz"
+    ).reset_index(drop=True)
 
-    processesed = pd.read_pickle(f"{env.DATA_FOLDER}preprocessed/{event}/{caseid}.gz")
-
-    if input("Stacked? (y/n)") == "y":
-        original_pre_ag = original[original.index < processesed.index.min()]
-        original_post_ag = original[original.index > processesed.index.max()]
-
-        # Find IOH
-        mask = processesed["Solar8000/ART_MBP"].lt(65)
-        idxs = processesed.index[
-            mask.rolling(window=60 * env.SAMPLING_RATE, axis=0).apply(
-                lambda x: x.all(), raw=True
-            )
-            == True
-        ]
+    if input("IOH events? (y/n)") == "y":  # Find IOH
+        mask = preprocessed["Solar8000/ART_MBP"].lt(65)
+        idxs = mask.rolling(window=60 * env.SAMPLING_RATE, axis=0).apply(
+            lambda x: x.all(), raw=True
+        )
+        preprocessed["IOH"] = idxs
+        preprocessed.fillna(0, inplace=True)
+        preprocessed["IOH"] *= 20
 
         fig, ax = plt.subplots(figsize=(15, 6))
 
-        original_pre_ag.plot(ax=ax, y="SNUADC/ART", color="gray", label=None)
-        original_post_ag.plot(ax=ax, y="SNUADC/ART", color="gray", label=None)
+        preprocessed.plot(ax=ax, y=["SNUADC/ART", "Solar8000/ART_MBP"], label=["Arterial pressure waveform", "MAP"])
+        preprocessed.plot(ax=ax, y="IOH", label="IOH", color="red")
 
-        processesed.plot(
-            ax=ax,
+        plt.title(f"IOH for {caseid}")
+        plt.xlabel("Time (ms)")
+        plt.ylabel("Arterial pressure (mmHg)")
+        plt.ylim((-50, 200))
+
+    else:
+        vtf = VitalFile(f"{env.DATA_FOLDER}vital/{caseid}.vital")
+        original = vtf.to_pandas(
+            [
+                "SNUADC/ART",
+                "Solar8000/ART_MBP",
+                "Solar8000/ART_DBP",
+                "Solar8000/ART_SBP",
+            ],
+            1 / env.SAMPLING_RATE,
+        )
+        original.ffill(inplace=True)
+        fig, axs = plt.subplots(2, 1, sharex=True, figsize=(15, 6))
+        original.plot(
+            ax=axs[0],
+            y=["SNUADC/ART"],
+            title="Original",
+            label=["Arterial pressure waveform"],
+            ylim=(-50, 200),
+            xlabel="Time (ms)",
+            ylabel="Arterial pressure (mmHg)",
+        )
+        preprocessed.plot(
+            ax=axs[1],
             y=[
                 "SNUADC/ART",
                 "Solar8000/ART_MBP",
                 "Solar8000/ART_DBP",
                 "Solar8000/ART_SBP",
             ],
+            title="Processed",
             label=[
                 "Arterial pressure waveform",
                 "Mean arterial pressure",
                 "DBP",
                 "SBP",
             ],
-        )
-
-        # Highlight all segments with hypotensive (IOH) events
-        hypotensive_event_patch = mpatches.Patch(
-            color="red", alpha=0.3, label="Hypotensive event (1 minute wide)"
-        )
-
-        if not idxs.empty:
-            idx_prev = 0
-            for i, idx in enumerate(idxs):
-                if idx - idx_prev >= 60 * env.SAMPLING_RATE:
-                    label = "Hypotensive event (1 minute wide)" if i == 0 else None
-                    ax.axvspan(
-                        idx - 60 * env.SAMPLING_RATE,
-                        idx,
-                        color="red",
-                        alpha=0.3,
-                        label=label,
-                    )
-                    idx_prev = idx
-        # Add the custom legend
-        current_handles, current_labels = ax.get_legend_handles_labels()
-        current_handles.append(hypotensive_event_patch)
-        ax.legend(handles=current_handles, labels=current_labels, loc="upper right")
-        plt.xlabel("Time (ms)")
-        plt.ylabel("Arterial pressure (mmHg)")
-        plt.ylim((-50, 200))
-
-    else:
-        fig, axs = plt.subplots(2, 1, sharex=True, figsize=(15, 6))
-        original.plot(
-            ax=axs[0],
-            y=[
-                "SNUADC/ART"
-            ],
-            title="Original",
-            label=[
-                "Arterial pressure waveform"
-            ],
-            ylim=(-50, 200),
-            xlabel="Time (ms)",
-            ylabel="Arterial pressure (mmHg)",
-        )
-        processesed.plot(
-            ax=axs[1],
-            y=["SNUADC/ART", "Solar8000/ART_MBP", "Solar8000/ART_DBP", "Solar8000/ART_SBP"],
-            title="Processed",
-            label=["Arterial pressure waveform", "Mean arterial pressure", "DBP", "SBP"],
             ylim=(-50, 200),
             xlabel="Time (ms)",
             ylabel="Arterial pressure (mmHg)",
