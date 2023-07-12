925e239a786e2121ca15472b4075e943274986f6
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-07-12 15:16:09 +0200

    change to module (wip)

--- plotting.py
+++ plotting.py
@@ -1,99 +1,104 @@
-from os.path import exists
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from vitaldb import VitalFile
-
 import json
+from os.path import exists
+
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import pandas as pd
+from vitaldb import VitalFile
 
 from utils import *
 
-timeframe = env.SAMPLING_RATE * 60
-plotting = True
 
-while plotting:
-    # Case selection
-    try:
-        caseid = int(input("Case ID: "))
-    except ValueError:
-        break
+def plot():
+    timeframe = env.SAMPLING_RATE * 60
+    plotting = True
+    while plotting:
+        # Case selection
+        try:
+            caseid = int(input("Case ID: "))
+        except ValueError:
+            break
 
-    if not exists(f"{env.DATA_FOLDER}vital/{caseid}.vital"):
-        print("Case does not exist (missing vital file)")
-        continue
+        if not exists(f"{env.DATA_FOLDER}vital/{caseid}.vital"):
+            print("Case does not exist (missing vital file)")
+            continue
 
-    if exists(f"{env.DATA_FOLDER}preprocessed/event/{caseid}.gz"):
-        event = "event"
-    elif exists(f"{env.DATA_FOLDER}preprocessed/nonevent/{caseid}.gz"):
-        event = "nonevent"
-    else:
-        print("Not pickled")
-        continue
+        if exists(f"{env.DATA_FOLDER}preprocessed/event/{caseid}.gz"):
+            event = "event"
+        elif exists(f"{env.DATA_FOLDER}preprocessed/nonevent/{caseid}.gz"):
+            event = "nonevent"
+        else:
+            print("Not pickled")
+            continue
 
-    # read pickled dataframes
-    preprocessed = pd.read_pickle(
-        f"{env.DATA_FOLDER}preprocessed/{event}/{caseid}.gz"
-    ).reset_index(drop=True)
+        # read pickled dataframes
+        preprocessed = pd.read_pickle(
+            f"{env.DATA_FOLDER}preprocessed/{event}/{caseid}.gz"
+        ).reset_index(drop=True)
 
-    if input("IOH events? (y/n)") == "y":  # Find IOH
-        mask = preprocessed["Solar8000/ART_MBP"].lt(65)
-        idxs = mask.rolling(window=60 * env.SAMPLING_RATE, axis=0).apply(
-            lambda x: x.all(), raw=True
-        )
-        preprocessed["IOH"] = idxs
-        preprocessed.fillna(0, inplace=True)
-        preprocessed["IOH"] *= 20
+        if input("IOH events? (y/n)") == "y":  # Find IOH
+            mask = preprocessed["Solar8000/ART_MBP"].lt(65)
+            idxs = mask.rolling(window=60 * env.SAMPLING_RATE, axis=0).apply(
+                lambda x: x.all(), raw=True
+            )
+            preprocessed["IOH"] = idxs
+            preprocessed.fillna(0, inplace=True)
+            preprocessed["IOH"] *= 20
 
-        fig, ax = plt.subplots(figsize=(15, 6))
+            fig, ax = plt.subplots(figsize=(15, 6))
 
-        preprocessed.plot(ax=ax, y=["SNUADC/ART", "Solar8000/ART_MBP"], label=["Arterial pressure waveform", "MAP"])
-        preprocessed.plot(ax=ax, y="IOH", label="IOH", color="red")
+            preprocessed.plot(
+                ax=ax,
+                y=["SNUADC/ART", "Solar8000/ART_MBP"],
+                label=["Arterial pressure waveform", "MAP"],
+            )
+            preprocessed.plot(ax=ax, y="IOH", label="IOH", color="red")
 
-        plt.title(f"IOH for {caseid}")
-        plt.xlabel("Time (ms)")
-        plt.ylabel("Arterial pressure (mmHg)")
-        plt.ylim((-50, 200))
+            plt.title(f"IOH for {caseid}")
+            plt.xlabel("Time (ms)")
+            plt.ylabel("Arterial pressure (mmHg)")
+            plt.ylim((-50, 200))
 
-    else:
-        vtf = VitalFile(f"{env.DATA_FOLDER}vital/{caseid}.vital")
-        original = vtf.to_pandas(
-            [
-                "SNUADC/ART",
-                "Solar8000/ART_MBP",
-                "Solar8000/ART_DBP",
-                "Solar8000/ART_SBP",
-            ],
-            1 / env.SAMPLING_RATE,
-        )
-        original.ffill(inplace=True)
-        fig, axs = plt.subplots(2, 1, sharex=True, figsize=(15, 6))
-        original.plot(
-            ax=axs[0],
-            y=["SNUADC/ART"],
-            title="Original",
-            label=["Arterial pressure waveform"],
-            ylim=(-50, 200),
-            xlabel="Time (ms)",
-            ylabel="Arterial pressure (mmHg)",
-        )
-        preprocessed.plot(
-            ax=axs[1],
-            y=[
-                "SNUADC/ART",
-                "Solar8000/ART_MBP",
-                "Solar8000/ART_DBP",
-                "Solar8000/ART_SBP",
-            ],
-            title="Processed",
-            label=[
-                "Arterial pressure waveform",
-                "Mean arterial pressure",
-                "DBP",
-                "SBP",
-            ],
-            ylim=(-50, 200),
-            xlabel="Time (ms)",
-            ylabel="Arterial pressure (mmHg)",
-        )
+        else:
+            vtf = VitalFile(f"{env.DATA_FOLDER}vital/{caseid}.vital")
+            original = vtf.to_pandas(
+                [
+                    "SNUADC/ART",
+                    "Solar8000/ART_MBP",
+                    "Solar8000/ART_DBP",
+                    "Solar8000/ART_SBP",
+                ],
+                1 / env.SAMPLING_RATE,
+            )
+            original.ffill(inplace=True)
+            fig, axs = plt.subplots(2, 1, sharex=True, figsize=(15, 6))
+            original.plot(
+                ax=axs[0],
+                y=["SNUADC/ART"],
+                title="Original",
+                label=["Arterial pressure waveform"],
+                ylim=(-50, 200),
+                xlabel="Time (ms)",
+                ylabel="Arterial pressure (mmHg)",
+            )
+            preprocessed.plot(
+                ax=axs[1],
+                y=[
+                    "SNUADC/ART",
+                    "Solar8000/ART_MBP",
+                    "Solar8000/ART_DBP",
+                    "Solar8000/ART_SBP",
+                ],
+                title="Processed",
+                label=[
+                    "Arterial pressure waveform",
+                    "Mean arterial pressure",
+                    "DBP",
+                    "SBP",
+                ],
+                ylim=(-50, 200),
+                xlabel="Time (ms)",
+                ylabel="Arterial pressure (mmHg)",
+            )
 
-    plt.show()
+        plt.show()
--- sktime_rocket.py
+++ sktime_rocket.py
@@ -102,22 +102,22 @@ def train_sgd(
 
 
 # %% TRAIN
+if __name__ == "__main__":
+    model = input("Model name: ")
 
-model = input("Model name: ")
+    if not exists(f"models/model_{model}/"):
+        verbose("No model found, creating one now...")
 
-if not exists(f"models/model_{model}/"):
-    verbose("No model found, creating one now...")
+        pipe, classifier = train_sgd(join(env.DATA_FOLDER, "ready"), epochs=3)
 
-    pipe, classifier = train_sgd(join(env.DATA_FOLDER, "ready"), epochs=3)
-
-    mlflow_sktime.save_model(pipe, f"models/model_{model}/pipeline/")
-    mlflow_sktime.save_model(classifier, f"models/model_{model}/classifier/")
-else:
-    verbose("Loading pipeline from models folder...")
-    pipe = mlflow_sktime.load_model(f"models/model_{model}/pipeline/")
-    verbose("Loading model from models folder...")
-    classifier = mlflow_sktime.load_model(f"models/model_{model}/classifier/")
-    verbose("Done.")
+        mlflow_sktime.save_model(pipe, f"models/model_{model}/pipeline/")
+        mlflow_sktime.save_model(classifier, f"models/model_{model}/classifier/")
+    else:
+        verbose("Loading pipeline from models folder...")
+        pipe = mlflow_sktime.load_model(f"models/model_{model}/pipeline/")
+        verbose("Loading model from models folder...")
+        classifier = mlflow_sktime.load_model(f"models/model_{model}/classifier/")
+        verbose("Done.")
 
 
 # %% TEST
@@ -177,137 +177,138 @@ def test_model_multi(
     return Y_test, Y_scores
 
 
-n_test = int(input("Number of test files: "))
-Y_test, Y_scores = test_model_multi(join(env.DATA_FOLDER, "ready"), n_files=n_test)
+if __name__ == "__main__":
+    n_test = int(input("Number of test files: "))
+    Y_test, Y_scores = test_model_multi(join(env.DATA_FOLDER, "ready"), n_files=n_test)
 
 
-verbose("Computing model performances...")
-# ROC Curve
-fpr, tpr, auc_thresholds = roc_curve(Y_test, Y_scores)
-roc_auc = auc(fpr, tpr)
+    verbose("Computing model performances...")
+    # ROC Curve
+    fpr, tpr, auc_thresholds = roc_curve(Y_test, Y_scores)
+    roc_auc = auc(fpr, tpr)
 
-# gmean
-gmean = np.sqrt(tpr * (1 - fpr))
-index = np.argmax(gmean)
-gmean_threshold = auc_thresholds[index]
-Y_pred_gmean = Y_scores > gmean_threshold
-# f1 score
-# f1_thresholds = np.arange(auc_thresholds[-1], auc_thresholds[0], 0.01)
-f1_thresholds = auc_thresholds
-f1_scores = []
-for t in f1_thresholds:
-    y_pred = Y_scores > t
-    f1 = f1_score(Y_test, y_pred)
-    f1_scores.append(f1)
-f1_threshold = f1_thresholds[f1_scores.index(max(f1_scores))]
+    # gmean
+    gmean = np.sqrt(tpr * (1 - fpr))
+    index = np.argmax(gmean)
+    gmean_threshold = auc_thresholds[index]
+    Y_pred_gmean = Y_scores > gmean_threshold
+    # f1 score
+    # f1_thresholds = np.arange(auc_thresholds[-1], auc_thresholds[0], 0.01)
+    f1_thresholds = auc_thresholds
+    f1_scores = []
+    for t in f1_thresholds:
+        y_pred = Y_scores > t
+        f1 = f1_score(Y_test, y_pred)
+        f1_scores.append(f1)
+    f1_threshold = f1_thresholds[f1_scores.index(max(f1_scores))]
 
-# Confusion matrix
-cm = confusion_matrix(Y_test, Y_pred_gmean)
-cm_norm = confusion_matrix(Y_test, Y_pred_gmean, normalize="true")
-if cm.shape != (2, 2):
-    cm = np.pad(
-        cm,
-        pad_width=((0, 1), (0, 1)),
-        mode="constant",
-        constant_values=0,
+    # Confusion matrix
+    cm = confusion_matrix(Y_test, Y_pred_gmean)
+    cm_norm = confusion_matrix(Y_test, Y_pred_gmean, normalize="true")
+    if cm.shape != (2, 2):
+        cm = np.pad(
+            cm,
+            pad_width=((0, 1), (0, 1)),
+            mode="constant",
+            constant_values=0,
+        )
+        cm_norm = np.pad(
+            cm_norm,
+            pad_width=((0, 1), (0, 1)),
+            mode="constant",
+            constant_values=0,
+        )
+
+    # %% PLOT
+    verbose("Plotting test results.")
+    # Create dataframe for the confusion matrices
+    df_cm = pd.DataFrame(cm, index=[False, True], columns=[False, True])
+    df_cm_norm = pd.DataFrame(cm_norm, index=[False, True], columns=[False, True])
+
+    # Create subplots
+    fig = plt.figure(layout="constrained", figsize=(15, 15))
+    gs = GridSpec(3, 2, figure=fig)
+    ax1l = fig.add_subplot(gs[0, 0])
+    ax1r = fig.add_subplot(gs[0, 1])
+    ax2l = fig.add_subplot(gs[1, 0])
+    ax2r = fig.add_subplot(gs[1, 1])
+    ax3 = fig.add_subplot(gs[2, :])
+
+    # Create heatmap for raw counts
+    sns.heatmap(df_cm, annot=True, fmt="d", cmap="Blues", ax=ax1l)
+    ax1l.set_title("Confusion Matrix - Counts")
+    ax1l.set_xlabel("Predicted label")
+    ax1l.set_ylabel("True label")
+
+    # Create heatmap for percentages
+    sns.heatmap(df_cm_norm, annot=True, fmt=".2%", cmap="Greens", ax=ax1r)
+    ax1r.set_title("Confusion Matrix - Percentages")
+    ax1r.set_xlabel("Predicted label")
+    ax1r.set_ylabel("True label")
+
+    # Plot ROC and best threshold point
+    ax2l.plot(fpr, tpr, color="blue", label="ROC curve (AUC = %0.2f)" % roc_auc)
+    ax2l.plot([0, 1], [0, 1], color="navy", linestyle="--")
+
+    ax2l.set_xlabel("1 - Specificity")
+    ax2l.set_ylabel("Sensitivity")
+    ax2l.set_title("Receiver Operating Characteristic (ROC)")
+    ax2l.legend(loc="lower right")
+
+    # f1 score and gmean by threshold
+    ax2r.plot(f1_thresholds, f1_scores, label="F1 score", color="green")
+    ax2r.axvline(
+        x=f1_threshold,
+        color="lightgreen",
+        label="Best f1 threshold %.2f : %.2f" % (f1_threshold, max(f1_scores)),
     )
-    cm_norm = np.pad(
-        cm_norm,
-        pad_width=((0, 1), (0, 1)),
-        mode="constant",
-        constant_values=0,
+    ax2r.plot(auc_thresholds, gmean, label="Gmean", color="blue")
+    ax2r.axvline(
+        x=gmean_threshold,
+        color="lightblue",
+        label="Best gmean threshold %.2f : %.2f" % (gmean_threshold, max(gmean)),
+    )
+    ax2r.legend()
+    ax2r.set_xlabel("Threshold")
+    ax2r.set_ylabel("F1 score and gmean")
+    ax2r.set_xlim(
+        [
+            -1.5 * abs(min(f1_threshold, gmean_threshold)),
+            1.5 * abs(max(f1_threshold, gmean_threshold)),
+        ]
+    )
+    ax2r.set_title("F1 score and gmean by threshold")
+
+    # Plot predictions with gmean threshold
+    Y_test = Y_test.astype(int)
+    ax3.scatter(
+        Y_test.index,
+        Y_test,
+        color="navy",
+        label="True labels",
+    )
+    ax3.scatter(
+        Y_test.index,
+        Y_pred_gmean,
+        color="cyan",
+        label="Predictions with best gmean threshold",
+    )
+    ax3.fill_between(
+        Y_test.index,
+        0,
+        Y_test,
+        where=(Y_test == 1) & (Y_pred_gmean == 1),
+        color="green",
+        alpha=0.5,
+    )
+    true_positive_zone = mpatches.Patch(color="green", alpha=0.5)
+    current_handles, current_labels = ax3.get_legend_handles_labels()
+    ax3.legend(
+        handles=current_handles.append(true_positive_zone),
+        labels=current_labels.append("True positive"),
+    )
+    ax3.set_title(
+        f"Predictions with gmean threshold : {Y_test.index[-1] * 20 / 3600:.2f}hrs"
     )
 
-# %% PLOT
-verbose("Plotting test results.")
-# Create dataframe for the confusion matrices
-df_cm = pd.DataFrame(cm, index=[False, True], columns=[False, True])
-df_cm_norm = pd.DataFrame(cm_norm, index=[False, True], columns=[False, True])
-
-# Create subplots
-fig = plt.figure(layout="constrained", figsize=(15, 15))
-gs = GridSpec(3, 2, figure=fig)
-ax1l = fig.add_subplot(gs[0, 0])
-ax1r = fig.add_subplot(gs[0, 1])
-ax2l = fig.add_subplot(gs[1, 0])
-ax2r = fig.add_subplot(gs[1, 1])
-ax3 = fig.add_subplot(gs[2, :])
-
-# Create heatmap for raw counts
-sns.heatmap(df_cm, annot=True, fmt="d", cmap="Blues", ax=ax1l)
-ax1l.set_title("Confusion Matrix - Counts")
-ax1l.set_xlabel("Predicted label")
-ax1l.set_ylabel("True label")
-
-# Create heatmap for percentages
-sns.heatmap(df_cm_norm, annot=True, fmt=".2%", cmap="Greens", ax=ax1r)
-ax1r.set_title("Confusion Matrix - Percentages")
-ax1r.set_xlabel("Predicted label")
-ax1r.set_ylabel("True label")
-
-# Plot ROC and best threshold point
-ax2l.plot(fpr, tpr, color="blue", label="ROC curve (AUC = %0.2f)" % roc_auc)
-ax2l.plot([0, 1], [0, 1], color="navy", linestyle="--")
-
-ax2l.set_xlabel("1 - Specificity")
-ax2l.set_ylabel("Sensitivity")
-ax2l.set_title("Receiver Operating Characteristic (ROC)")
-ax2l.legend(loc="lower right")
-
-# f1 score and gmean by threshold
-ax2r.plot(f1_thresholds, f1_scores, label="F1 score", color="green")
-ax2r.axvline(
-    x=f1_threshold,
-    color="lightgreen",
-    label="Best f1 threshold %.2f : %.2f" % (f1_threshold, max(f1_scores)),
-)
-ax2r.plot(auc_thresholds, gmean, label="Gmean", color="blue")
-ax2r.axvline(
-    x=gmean_threshold,
-    color="lightblue",
-    label="Best gmean threshold %.2f : %.2f" % (gmean_threshold, max(gmean)),
-)
-ax2r.legend()
-ax2r.set_xlabel("Threshold")
-ax2r.set_ylabel("F1 score and gmean")
-ax2r.set_xlim(
-    [
-        -1.5 * abs(min(f1_threshold, gmean_threshold)),
-        1.5 * abs(max(f1_threshold, gmean_threshold)),
-    ]
-)
-ax2r.set_title("F1 score and gmean by threshold")
-
-# Plot predictions with gmean threshold
-Y_test = Y_test.astype(int)
-ax3.scatter(
-    Y_test.index,
-    Y_test,
-    color="navy",
-    label="True labels",
-)
-ax3.scatter(
-    Y_test.index,
-    Y_pred_gmean,
-    color="cyan",
-    label="Predictions with best gmean threshold",
-)
-ax3.fill_between(
-    Y_test.index,
-    0,
-    Y_test,
-    where=(Y_test == 1) & (Y_pred_gmean == 1),
-    color="green",
-    alpha=0.5,
-)
-true_positive_zone = mpatches.Patch(color="green", alpha=0.5)
-current_handles, current_labels = ax3.get_legend_handles_labels()
-ax3.legend(
-    handles=current_handles.append(true_positive_zone),
-    labels=current_labels.append("True positive"),
-)
-ax3.set_title(
-    f"Predictions with gmean threshold : {Y_test.index[-1] * 20 / 3600:.2f}hrs"
-)
-
-plt.show()
+    plt.show()
